ea9efdf109809b0b4447d8dce4fa7bee30950518
Erich <35262203+Engeloid@users.noreply.github.com>
2023-12-05 11:41:07 +0100

    simplified main for duscussion purposes

--- src/main.go
+++ src/main.go
@@ -3,59 +3,19 @@
 import (
 	"fmt"
 	"log"
-	"math/big"
-	"os"
 	. "rollup/src/gnark_rollup"
-	. "rollup/src/tools"
+
+	// . "rollup/src/tools"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/tidwall/gjson"
-)
-
-const (
-	fileToProof = "./test/cubicg16.proof"
-	fileToPK    = "./test/cubicg16.pk"
-	fileToCS    = "./test/cubicg16.cs"
 )
 
 func main() {
-	// Initial setup
-	var proof groth16.Proof
-	var err error
-	if FileExists(fileToProof) || FileExists(fileToPK) || FileExists(fileToCS) {
-		fmt.Println("Proof files will be created")
-		// Setup and generate files (solidity contract, pk, etc.)
-		proof, err = SetupCubicProofAndContract(fileToProof, fileToPK, fileToCS) // Cubic example
-		if err != nil {
-			log.Fatalf("Error setting up proof and contract files: %v", err)
-		}
-	} else {
-		// Load proof
-		proof, err = LoadProofFromFile(fileToProof)
-		if err != nil {
-			log.Fatalf("Error loading proof: %v", err)
-		} else {
-			fmt.Println("Proof loaded from", fileToProof)
-			// Now you can use the 'proof' variable in the code.
-		}
-	}
-
-	// ----------------------- Main part -------------------
-	// var shift uint64 = 256
 	var amount uint64 = 10
 	var operatorA, privKeys = CreateOP(10)
-
-	/* fmt.Printf("Index of 1st account: %d\n", operatorA.State[0:32])
-	fmt.Printf("Nonce of 1st account: %d\n", operatorA.State[32:64])
-	fmt.Printf("Balance of 1st account: %d\n", operatorA.State[64:96])
-	fmt.Printf("Balance of 2nd account: %d\n", operatorA.State[shift-32:shift])
-	fmt.Printf("Public keys of accounts: %x\n", operatorA.AccountMap)
-	fmt.Printf("Private key of 1st account: %x\n\n", privKeys[0]) */
 
 	// get info on both parties of the transaction
 	sender, err := operatorA.ReadAcc(0)
@@ -67,11 +27,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error reading receiver account: %v", err)
 	}
-
-	/* fmt.Printf("sender account: %x\n", sender.PubKey)
-	fmt.Printf("sender account A.X: %x\n", sender.PubKey.A.X)
-	fmt.Printf("sender account A.Y: %x\n", sender.PubKey.A.Y)
-	fmt.Printf("sender account A.X.Bytes(): %x\n", sender.PubKey.A.X.Bytes()) */
 
 	// Transactions
 
@@ -108,7 +63,7 @@ func main() {
 		log.Fatalf("Could not create public witness: %v", err)
 	}
 	// generate the proof
-	proof, err = groth16.Prove(r1cs, pk, witness)
+	proof, err := groth16.Prove(r1cs, pk, witness)
 	if err != nil {
 		log.Fatalf("Could not create proof using pk and witnesses: %v", err)
 	}
@@ -213,52 +168,4 @@ func main() {
 	assignment.RootHashesBefore = operatorA.Witnesses.RootHashesBefore
 	assignment.RootHashesAfter = operatorA.Witnesses.RootHashesAfter */
 
-	// ------------------ Proof (Cubic) --------------------
-	eth_proof, _ := ProofToEthereumProof(proof)
-
-	// ---------------- Truffle/Ganache contract call --------
-	// Connect to your Ethereum node (Ganache instance or other network)
-	client, err := ethclient.Dial("HTTP://127.0.0.1:7545") // Update with your Ganache URL
-	if err != nil {
-		log.Fatalf("Failed to connect to ethclient/ganache: %v", err)
-	}
-	defer client.Close()
-
-	// Read contract address from migration artefact json
-	verifier_json, err := os.ReadFile("build/contracts/Verifier.json")
-	if err != nil {
-		log.Fatalf("Error reading JSON file: %v\n", err)
-	}
-	json_contract_address := gjson.Get(string(verifier_json), "networks.5777.address")
-	contractAddress := common.HexToAddress(json_contract_address.String())
-
-	// Create a contract instance
-	verifier, err := NewVerifier(contractAddress, client)
-	if err != nil {
-		log.Fatalf("Failed to instantiate Verifier contract: %v", err)
-	}
-
-	// Call the contract function
-	input_value := [1]*big.Int{}
-	input_value[0] = new(big.Int).SetUint64(35)
-	fmt.Println("Input y=", input_value)
-
-	//var result []interface{}
-	result, err := verifier.VerifyProof(nil, eth_proof.A, eth_proof.B, eth_proof.C, input_value)
-	if err != nil {
-		log.Fatalf("Failed to retrieve value: %v", err)
-	}
-
-	fmt.Println("Function Result:", result)
-
-	/*
-		// generate assets
-		// gnark-solidity-checker generate --dir tmpdir --solidity contract_g16.sol
-		cmd := exec.Command("gnark-solidity-checker", "generate", "--dir", tmpDir, "--solidity", "gnark_verifier.sol")
-		out, err := cmd.CombinedOutput()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		fmt.Printf("out: %x\n", out)
-	*/
 }
